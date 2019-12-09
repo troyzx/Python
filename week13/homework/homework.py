b4be342932ab06ee28e038071851db07666b9843
Troy <troyzx@icloud.com>
2019-12-09 08:48:14 +0800

    prone

--- week13/homework/homework.py
+++ week13/homework/homework.py
@@ -1,15 +1,6 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
-# %% Change working directory from the workspace root to the ipynb file
-# location. Turn this addition off with the DataScience.
-# changeDirOnImportExport setting
-# ms-python.python added
-import os
-try:
-    os.chdir(os.path.join(os.getcwd(), 'week13/homework'))
-    print(os.getcwd())
-except:
-    pass
+
 # %%
 import numpy as np
 from matplotlib import pyplot as plt
